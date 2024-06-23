4d9940db0ef189b364d4d4bbec03d716be7353f3
Peter Njuguna <njugunapm23@gmail.com>
2024-06-23 18:36:02 +0300

    Initial commit

---
+++ src/components/footer/Footer.js
@@ -0,0 +1,21 @@
+import React from "react";
+import "./Footer.css";
+import { Fade } from "react-reveal";
+import { greeting } from "../../portfolio.js";
+/* eslint-disable jsx-a11y/accessible-emoji */
+
+// Get current year
+const currentYear = new Date().getFullYear();
+
+export default function Footer(props) {
+  return (
+    <div className="footer-div">
+      <Fade>
+        <p className="footer-text" style={{ color: props.theme.secondaryText }}>
+          Made with <span role="img">❤️</span> by {greeting.title} All Rights Reserved &copy; {currentYear}
+        </p>
+        {/* <ToggleSwitch theme={props.theme} onToggle={props.onToggle}/> */}
+      </Fade>
+    </div>
+  );
+}
