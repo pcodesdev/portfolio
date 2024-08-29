547b89b580c958233095760af95414ea8e30a1ab
Peter Njuguna <njugunapm23@gmail.com>
2024-08-29 23:04:05 +0300

    have edited the work experience section

--- src/portfolio.js
+++ src/portfolio.js
@@ -593,7 +593,7 @@ const experience = {
   title: "Experience",
   subtitle: "Work, Internship and Volunteership",
   description:
-    "I have worked with many evolving startups as ML and DL Developer, Designer and Software Architect. I have also worked with some well established companies mostly as AI Developer. I love organising events and that is why I am also involved with many opensource communities as a representative.",
+    "With over a decade of diverse experience, I've made significant contributions across various sectors. As the founder of Pemanju IT Consultancy Services, I offer fullstack development expertise to clients. My portfolio includes project management and digital skills training for NGOs, as well as tech support and data analysis for both private and public organizations. I'm also actively involved in open-source projects, leveraging my skills as a developer and consultant to drive innovation. This multifaceted background allows me to bring a unique blend of technical prowess and strategic insight to every project I undertake.",
   header_image_path: "experience.svg",
   sections: [
     {
@@ -601,47 +601,50 @@ const experience = {
       work: true,
       experiences: [
         {
-          title: "Machine Learning Engineer",
-          company: "TikTok Inc.",
-          company_url: "https://www.tiktok.com/en/",
-          logo_path: "tiktok_logo.png",
-          duration: "June 2023 - Present",
-          location: "San Jose, CA, USA",
+          title: "Project Assistant",
+          company: "Finn Church Aid",
+          company_url:
+            "https://www.linkedin.com/company/finn-church-aid/mycompany/",
+          logo_path: "fca.png",
+          duration: "January 2022 - Present",
+          location: "Nairobi, Kenya",
           description:
-            "Improving ads ranking models on the core TikTok product. Experience working on modeling two-tower architectures like DeepFM, Wide & deep learning, etc. Working on Large Language Models (LLM) pretraining and Large Multi-modal Model (LMM) finetuning strategies.",
+            " I lead and coordinate a team of 6 technical coaches to deliver comprehensive digital skills training to project participants. I facilitate smooth project operations by ensuring alignment between coaching efforts and project goals, while also providing hands-on technical support to participants to enhance their learning experience. Through proactive collaboration and problem-solving, I continuously improve the effectiveness and efficiency of the digital skills training program.",
           color: "#000000",
         },
         {
-          title: "Associate AI Engineer",
-          company: "Legato Health Technology",
-          company_url: "https://legatohealthtech.com/",
-          logo_path: "legato_logo.png",
-          duration: "June 2020 - Aug 2021",
-          location: "Hyderabad, Telangana",
-          description:
-            "I am working on automating healthcare products. The projects involve automation for process improvements and for significantly enhancing the profits. I am currently working on Cancer Survival and Reoccurence Prediction. Our goal is to make AI system which scales and removes doctor dependency as much as possible.",
-          color: "#0879bf",
-        },
-        {
-          title: "Android and ML Developer",
-          company: "Muffito Incorporation",
-          company_url: "https://www.linkedin.com/company/muffito-inc/about/",
-          logo_path: "muffito_logo.png",
+          title: "Fullstack Developer",
+          company: "Pemanju IT Consulting Services",
+          company_url:
+            "https://www.linkedin.com/company/pemanju-it-consultancy-services/",
+          logo_path: "pemanju.jpeg",
           duration: "May 2018 - Oct 2018",
-          location: "Pune, Maharashtra",
+          location: "Nairobi, Kenya",
           description:
-            "I have created complete Android Application for locating Pub, Bar and beverage shops around you. I have also worked on implementation of algorithms for Face Detection, Text extraction from Image. I was involved in a team for creating complete software architecure of mobile and web application as well as admin panel for company.",
+            "As the founder of Pemanju IT Consulting Services, I spearhead fullstack development projects, delivering tailored solutions to diverse clients. I architect robust web applications, integrating cutting-edge frontend and backend technologies to meet complex business requirements. Additionally, I collaborate closely with clients to analyze their needs, devise strategic technology roadmaps, and implement scalable, efficient systems that drive their digital transformation initiatives.",
           color: "#9b1578",
         },
         {
-          title: "Android Developer",
-          company: "FreeCopy Pvt. Ltd.",
-          company_url: "https://www.linkedin.com/company/freecopy/about/",
-          logo_path: "freecopy_logo.png",
-          duration: "Nov 2017 - Dec 2017",
-          location: "Ahmedabad, Gujarat",
+          title: "Data Specialist",
+          company: "Realfield",
+          company_url: "https://www.linkedin.com/company/realfield-limited/",
+          logo_path: "realfield.jpeg",
+          duration: "March 2021 - December2021",
+          location: "Naivasha, Kenya",
           description:
-            "FreeCopy is the Start up from Indian Institute of Management, Ahmedabad. I have changed the integration of the whole app from Google to Firebase. I learnt the efﬁcient ways of Data communications like Retroﬁt, Eventbus etc. I experienced the real time start up. I learnt the Design thinking of UI on perspective of People.",
+            "Orchestrated comprehensive data collection initiatives, implementing rigorous verification processes to ensure data integrity and accuracy across diverse datasets. Spearheaded cross-functional collaboration to establish and maintain high data quality standards, while leveraging advanced analysis tools to enhance data usability. Engineered automated data cleaning scripts and workflows, significantly streamlining processes and boosting overall efficiency in data management operations.",
+          color: "#0879bf",
+        },
+
+        {
+          title: "IT Technical Support Specialist",
+          company: "Kenya Power and Lighting Company",
+          company_url: "https://www.linkedin.com/company/kenya-power",
+          logo_path: "kplc.png",
+          duration: "Nov 2017 - Dec 2017",
+          location: "Nakuru, Kenya",
+          description:
+            "At Kenya Power and Lighting Company, I spearheaded the implementation of a robust IT support system, significantly reducing downtime and enhancing operational efficiency. I orchestrated comprehensive training programs for staff, elevating the company's overall technical proficiency and fostering a culture of digital innovation. Additionally, I pioneered the development and deployment of customized software solutions, streamlining internal processes and dramatically improving customer service delivery.",
           color: "#fc1f20",
         },
       ],
@@ -649,99 +652,100 @@ const experience = {
     {
       title: "Internships",
       experiences: [
-        {
-          title: "Machine Learning Intern",
-          company: "TikTok Inc.",
-          company_url: "https://www.tiktok.com/en/",
-          logo_path: "tiktok_logo.png",
-          duration: "May 2022 - Aug 2022",
-          location: "San Francisco, USA",
-          description:
-            "Building new features on the backend recommendation system, specifically ranking algorithms for Ads that touch hundreds of millions of people around the world. Improving online and offline content ranking algorithms by performing hard sample data replays for training steps.",
-          color: "#000000",
-        },
-        {
-          title: "Data Science Research Intern",
-          company: "Delhivery Pvt. Ltd.",
-          company_url: "https://www.delhivery.com/",
-          logo_path: "delhivery_logo.png",
-          duration: "May 2019 - Sept 2019",
-          location: "Gurgaon, Haryana",
-          description:
-            "I have worked on project of predicting freight rates based on previous data. There were two objectives: (1) To build a forecasting engine to predict daily freight rates. (2) To embed feature in the model which can explain the seasonal major changes in freight rate based on regions and locations. I have closely worked with deep learning models in combination with statistical methods to create solution for this. At the end of internship, I had created model deployed on AWS EC2 with the use of Kafka stream jobs, ElasticSearch and PostgreSQL.",
-          color: "#ee3c26",
-        },
-        {
-          title: "Data Science Intern",
-          company: "Intel Indexer LLC",
-          company_url:
-            "https://opencorporates.com/companies/us_dc/EXTUID_4170286",
-          logo_path: "intel_logo.jpg",
-          duration: "Nov 2018 - Dec 2018",
-          location: "Work From Home",
-          description:
-            "This is financial Solution Company. I have made Supervised Learning model for the company which can perform time series analysis on Stock price data for 32 companies. I have built LSTM Neural Networks Model and trained the data of 32 companies for last 2 years. This model is also used for forecasting.",
-          color: "#0071C5",
-        },
+        // At the moment I have no internship engagements
+        // {
+        //   title: "Machine Learning Intern",
+        //   company: "TikTok Inc.",
+        //   company_url: "https://www.tiktok.com/en/",
+        //   logo_path: "tiktok_logo.png",
+        //   duration: "May 2022 - Aug 2022",
+        //   location: "San Francisco, USA",
+        //   description:
+        //     "Building new features on the backend recommendation system, specifically ranking algorithms for Ads that touch hundreds of millions of people around the world. Improving online and offline content ranking algorithms by performing hard sample data replays for training steps.",
+        //   color: "#000000",
+        // },
+        // {
+        //   title: "Data Science Research Intern",
+        //   company: "Delhivery Pvt. Ltd.",
+        //   company_url: "https://www.delhivery.com/",
+        //   logo_path: "delhivery_logo.png",
+        //   duration: "May 2019 - Sept 2019",
+        //   location: "Gurgaon, Haryana",
+        //   description:
+        //     "I have worked on project of predicting freight rates based on previous data. There were two objectives: (1) To build a forecasting engine to predict daily freight rates. (2) To embed feature in the model which can explain the seasonal major changes in freight rate based on regions and locations. I have closely worked with deep learning models in combination with statistical methods to create solution for this. At the end of internship, I had created model deployed on AWS EC2 with the use of Kafka stream jobs, ElasticSearch and PostgreSQL.",
+        //   color: "#ee3c26",
+        // },
+        // {
+        //   title: "Data Science Intern",
+        //   company: "Intel Indexer LLC",
+        //   company_url:
+        //     "https://opencorporates.com/companies/us_dc/EXTUID_4170286",
+        //   logo_path: "intel_logo.jpg",
+        //   duration: "Nov 2018 - Dec 2018",
+        //   location: "Work From Home",
+        //   description:
+        //     "This is financial Solution Company. I have made Supervised Learning model for the company which can perform time series analysis on Stock price data for 32 companies. I have built LSTM Neural Networks Model and trained the data of 32 companies for last 2 years. This model is also used for forecasting.",
+        //   color: "#0071C5",
+        // },
       ],
     },
     {
       title: "Volunteerships",
       experiences: [
-        {
-          title: "Google Explore ML Facilitator",
-          company: "Google",
-          company_url: "https://about.google/",
-          logo_path: "google_logo.png",
-          duration: "June 2019 - April 2020",
-          location: "Hyderabad, Telangana",
-          description:
-            "Explore Machine Learning (ML) is a Google-sponsored program for university students to get started with Machine Learning. The curriculum offers 3 tracks of ML Content (Beginner, Intermediate, Advanced) and relies on university student facilitators to train other students on campus and to build opensource projects under this program.",
-          color: "#4285F4",
-        },
-        {
-          title: "Microsoft Student Partner",
-          company: "Microsoft",
-          company_url: "https://www.microsoft.com/",
-          logo_path: "microsoft_logo.png",
-          duration: "Aug 2019 - May 2020",
-          location: "Hyderabad, Telangana",
-          description:
-            "Microsoft Student Partner is a program for university students to lead the awareness and use of Cloud especially Azure tools in the development of their projects and startups. Under this program, I have organised hands on workshops and seminars to teach Cloud Computing concepts to students.",
-          color: "#D83B01",
-        },
-        {
-          title: "Mozilla Campus Captain",
-          company: "Mozilla",
-          company_url: "https://www.mozilla.org/",
-          logo_path: "mozilla_logo.png",
-          duration: "Oct 2019 - May 2020",
-          location: "Kurnool, Andhra Pradesh",
-          description:
-            "My responsibility for this program was to create opensource environment in college and in the city. We have organised multiple hackathons on the problems collected by ordinary people from Kurnool city. We have build opensource community of our own college. The community is available at dsc_iiitdmk on github.",
-          color: "#000000",
-        },
-        {
-          title: "Developer Students Club Member",
-          company: "DSC IIITDM Kurnool",
-          company_url:
-            "https://www.linkedin.com/company/developer-students-club-iiitdm-kurnool",
-          logo_path: "dsc_logo.png",
-          duration: "Jan 2018 - May 2020",
-          location: "Kurnool, Andhra Pradesh",
-          description:
-            "We have well established developer club in college which is directly associated with Google Developers. We have developed many interdisciplinary projects under the membership of this club. We have organised workshops and activities on Android Application Development, Flutter and React JS.",
-          color: "#0C9D58",
-        },
+        // {
+        //   title: "Google Explore ML Facilitator",
+        //   company: "Google",
+        //   company_url: "https://about.google/",
+        //   logo_path: "google_logo.png",
+        //   duration: "June 2019 - April 2020",
+        //   location: "Hyderabad, Telangana",
+        //   description:
+        //     "Explore Machine Learning (ML) is a Google-sponsored program for university students to get started with Machine Learning. The curriculum offers 3 tracks of ML Content (Beginner, Intermediate, Advanced) and relies on university student facilitators to train other students on campus and to build opensource projects under this program.",
+        //   color: "#4285F4",
+        // },
+        // {
+        //   title: "Microsoft Student Partner",
+        //   company: "Microsoft",
+        //   company_url: "https://www.microsoft.com/",
+        //   logo_path: "microsoft_logo.png",
+        //   duration: "Aug 2019 - May 2020",
+        //   location: "Hyderabad, Telangana",
+        //   description:
+        //     "Microsoft Student Partner is a program for university students to lead the awareness and use of Cloud especially Azure tools in the development of their projects and startups. Under this program, I have organised hands on workshops and seminars to teach Cloud Computing concepts to students.",
+        //   color: "#D83B01",
+        // },
+        // {
+        //   title: "Mozilla Campus Captain",
+        //   company: "Mozilla",
+        //   company_url: "https://www.mozilla.org/",
+        //   logo_path: "mozilla_logo.png",
+        //   duration: "Oct 2019 - May 2020",
+        //   location: "Kurnool, Andhra Pradesh",
+        //   description:
+        //     "My responsibility for this program was to create opensource environment in college and in the city. We have organised multiple hackathons on the problems collected by ordinary people from Kurnool city. We have build opensource community of our own college. The community is available at dsc_iiitdmk on github.",
+        //   color: "#000000",
+        // },
+        // {
+        //   title: "Developer Students Club Member",
+        //   company: "DSC IIITDM Kurnool",
+        //   company_url:
+        //     "https://www.linkedin.com/company/developer-students-club-iiitdm-kurnool",
+        //   logo_path: "dsc_logo.png",
+        //   duration: "Jan 2018 - May 2020",
+        //   location: "Kurnool, Andhra Pradesh",
+        //   description:
+        //     "We have well established developer club in college which is directly associated with Google Developers. We have developed many interdisciplinary projects under the membership of this club. We have organised workshops and activities on Android Application Development, Flutter and React JS.",
+        //   color: "#0C9D58",
+        // },
         {
           title: "Developer Program Member",
           company: "Github",
           company_url: "https://github.com/",
           logo_path: "github_logo.png",
-          duration: "July 2019 - PRESENT",
-          location: "Work From Home",
+          duration: "July 2017 - Present",
+          location: "Remote",
           description:
-            "I am actively contributing to many opensource projects. I have contributed to projects of organisations like Tensorflow, Uber, Facebook, Google, Scikit-learn, Kiwix, Sympy, Python, NVLabs, Fossasia, Netrack, Keras etc. These contributions include bug fixes, feature requests and formulating proper documentation for project.",
+            "I actively contribute to a diverse array of open-source projects, leveraging cutting-edge technologies and best practices in software development. My contributions span from healthcare informatics in Hospital Management Systems to educational technology in Free Open Source Courses, as well as technical content creation through tech blogging and the development of this portfolio as an open-source, globally accessible resource for developers. These contributions encompass critical bug fixes, innovative feature implementations, and the creation of comprehensive documentation, all adhering to industry standards and employing modern development workflows such as Git version control and continuous integration/continuous deployment (CI/CD) pipelines.",
           color: "#181717",
         },
       ],
